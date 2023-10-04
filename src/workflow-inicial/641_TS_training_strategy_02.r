4b0530a9f1533beaf015ac00af12b1645f2ef8dc
juanstrubbia <jcstrubbia@gmail.com>
2023-10-04 20:41:04 +0000

    LOR 6

--- src/workflow-inicial/641_TS_training_strategy_02.r
+++ src/workflow-inicial/641_TS_training_strategy_02.r
@@ -18,21 +18,22 @@ PARAM$exp_input <- "FE6310"
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
 
-PARAM$future <- c(202109)
+PARAM$future <- c(202107)
 PARAM$final_train <- c(
-  202107, 202106, 202105, 202104, 202103, 202102,
-  202101, 202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912,
-  201911, 201910, 201909
+  202106, 202105, 202104, 202103, 202102, 202101,
+  202007, 202006, 202005, 202004, 202003,
+  201912, 201911, 201910, 201909, 201908, 201907, 201906
 )
 
 PARAM$train$training <- c(
-  202105, 202104, 202103, 202102, 202101,
-  202012, 202011, 202010, 202009, 202008, 202002, 202001, 201912, 201911,
-  201910, 201909, 201908, 201907
+  202102, 202101,
+  202007, 202006, 202005, 202004, 202003,
+  201912, 201911, 201910, 201909, 201908, 201907, 201906,
+  201905, 201904, 201903, 201902
 )
 
-PARAM$train$validation <- c(202106)
-PARAM$train$testing <- c(202107)
+PARAM$train$validation <- c(202103)
+PARAM$train$testing <- c(202104, 202105, 202106)
 
 # Atencion  0.1  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
