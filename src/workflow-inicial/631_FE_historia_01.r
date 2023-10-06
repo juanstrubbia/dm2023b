38e1c5460074effa968b76b650abd861a3927c69
juanstrubbia <jcstrubbia@gmail.com>
2023-10-06 14:19:23 +0000

    Origin changes

--- src/workflow-inicial/631_FE_historia_01.r
+++ src/workflow-inicial/631_FE_historia_01.r
@@ -21,18 +21,18 @@ PARAM$experimento <- "FE6310"
 
 PARAM$exp_input <- "DR6210"
 
-PARAM$lag1 <- FALSE
+PARAM$lag1 <- TRUE
 PARAM$lag2 <- TRUE
 PARAM$lag3 <- FALSE
 
-PARAM$Tendencias1$run <- FALSE
-PARAM$Tendencias1$ventana <- 9
+PARAM$Tendencias1$run <- TRUE
+PARAM$Tendencias1$ventana <- 6
 PARAM$Tendencias1$tendencia <- TRUE
 PARAM$Tendencias1$minimo <- FALSE
-PARAM$Tendencias1$maximo <- TRUE
-PARAM$Tendencias1$promedio <- TRUE
+PARAM$Tendencias1$maximo <- FALSE
+PARAM$Tendencias1$promedio <- FALSE
 PARAM$Tendencias1$ratioavg <- FALSE
-PARAM$Tendencias1$ratiomax <- TRUE
+PARAM$Tendencias1$ratiomax <- FALSE
 
 PARAM$Tendencias2$run <- FALSE
 PARAM$Tendencias2$ventana <- 6
@@ -45,15 +45,15 @@ PARAM$Tendencias2$ratiomax <- FALSE
 
 
 PARAM$RandomForest$run <- TRUE
-PARAM$RandomForest$num.trees <- 100
-PARAM$RandomForest$max.depth <- 8
+PARAM$RandomForest$num.trees <- 20
+PARAM$RandomForest$max.depth <- 4
 PARAM$RandomForest$min.node.size <- 1000
 PARAM$RandomForest$mtry <- 40
 PARAM$RandomForest$semilla <- 600037 # cambiar por la propia semilla
 
 
 # varia de 0.0 a 2.0, si es 0.0 NO se activan
-PARAM$CanaritosAsesinos$ratio <- 3.0
+PARAM$CanaritosAsesinos$ratio <- 0.0
 # desvios estandar de la media, para el cutoff
 PARAM$CanaritosAsesinos$desvios <- 4.0
 # cambiar por la propia semilla
