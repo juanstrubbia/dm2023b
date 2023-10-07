c49d3400d901c5bd79f82dc05867ee846d4a933d
juanstrubbia <jcstrubbia@gmail.com>
2023-10-07 00:04:12 +0000

    Corrida 5

--- src/workflow-inicial/641_TS_training_strategy_05.r
+++ src/workflow-inicial/641_TS_training_strategy_05.r
@@ -20,20 +20,20 @@ PARAM$exp_input <- "FE6310"
 
 PARAM$future <- c(202107)
 PARAM$final_train <- c(
-  202106, 202105, 202104, 202103, 202102, 202101,
+  202105, 202104, 202103, 202102, 202101,
   202012, 202011, 202010, 202009, 202008, 202002, 202001,
-  201912, 201911, 201910, 201909, 201908
+  201912, 201911, 201910, 201909, 201908, 202107
 )
 
 PARAM$train$training <- c(
-  202103, 202102, 202103,
+  202101,
   202012, 202011, 202010, 202009, 202008, 202002, 202001,
   201912, 201911, 201910, 201909, 201908, 201907, 201906,
-  201905
+  201905, 201904, 201903
 )
 
-PARAM$train$validation <- c(202104)
-PARAM$train$testing <- c(202105)
+PARAM$train$validation <- c(202102, 202104)
+PARAM$train$testing <- c(202103)
 
 # Atencion  0.1  de  undersampling de la clase mayoritaria,  los CONTINUA
 # 1.0 significa NO undersampling ,  0.1  es quedarse con el 10% de los CONTINUA
