7c1d7a9ca1ec5619cb5e7f87ef14498a743796ed
juanstrubbia <jcstrubbia@gmail.com>
2023-10-11 21:55:30 +0000

    auto commit 1       20231011 215529

--- src/workflow-semillerio/701_RUN_correr_workflow_1.r
+++ src/workflow-semillerio/701_RUN_correr_workflow_1.r
@@ -10,11 +10,11 @@ options(error = function() {
 # corrida de cada paso del workflow
 
 # primeros pasos, relativamente rapidos
-source("~/dm2023b/src/workflow-semillero/711_CA_reparar_dataset_1.r")
-source("~/dm2023b/src/workflow-semillero/721_DR_corregir_drifting_1.r")
-source("~/dm2023b/src/workflow-semillero/731_FE_historia_1.r")
-source("~/dm2023b/src/workflow-semillero/741_TS_training_strategy_1.r")
+source("~/dm2023b/src/workflow-semillerio/711_CA_reparar_dataset_1.r")
+source("~/dm2023b/src/workflow-semillerio/721_DR_corregir_drifting_1.r")
+source("~/dm2023b/src/workflow-semillerio/731_FE_historia_1.r")
+source("~/dm2023b/src/workflow-semillerio/741_TS_training_strategy_1.r")
 
 # ultimos pasos, muy lentos
-source("~/dm2023b/src/workflow-semillero/751_HT_lightgbm_1.r")
-source("~/dm2023b/src/workflow-semillero/795_ZZ_final_semillero_1.r")
+source("~/dm2023b/src/workflow-semillerio/751_HT_lightgbm_1.r")
+source("~/dm2023b/src/workflow-semillerio/795_ZZ_final_semillerio_1.r")
